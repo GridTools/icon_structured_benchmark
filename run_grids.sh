da02e453313421a6a8e93f482d7aa7a15d68444f
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-03-15 17:05:53 +0100

    Fix run_grid script

--- run_grids.sh
+++ run_grids.sh
@@ -9,7 +9,7 @@ source venv_py311_icon4py/bin/activate
 
 export PYTHONPATH=/Users/ioannmag/cscs_repos/cycle20/icon-structured/build:/Users/ioannmag/cscs_repos/serialbox/build/install/python:$PYTHONPATH
 
-for grid_file in ${GRIDS_DIRECTORY}/torus_100000_100000_*.nc;
+for grid_file in ${GRIDS_DIRECTORY}/torus_100000_100000_*.nc; do
     echo "Running grid ${grid_file}"
     grid_name=$(echo $grid_file | grep -E "torus_100000_100000_[0-9]*" -o)
     python3.11 run_torus_grid.py ${grid_file} --klevels $KLEVELS --repetitions $REPETITIONS --output $grid_name.pdf
