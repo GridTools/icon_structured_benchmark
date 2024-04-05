a51e2d987a544b8bfd8a8abfed3cc73d683f54c6
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-04-05 15:32:22 +0200

    Revert "Revert "Initialize target views in constructor""
    This reverts commit 3c65d6bc2a9a8d6c1e3c9f7c3ffdd02240214143.

--- include/common.hpp
+++ include/common.hpp
@@ -30,18 +30,18 @@ struct nabla4_data {
     std::size_t ECVDim;
 
     using data_store_2d_t =
-        decltype(gridtools::storage::builder<traits_t>.dimensions(0, 0).template type<double>().build());
+        decltype(gridtools::storage::builder<traits_t>.dimensions(0, 0).template type<double>().build()->target_view());
     using data_store_1d_t =
-        decltype(gridtools::storage::builder<traits_t>.dimensions(0).template type<double>().build());
+        decltype(gridtools::storage::builder<traits_t>.dimensions(0).template type<double>().build()->target_view());
 
-    data_store_2d_t u_vert_gt;
-    data_store_2d_t v_vert_gt;
-    data_store_1d_t primal_normal_vert_v1_gt;
-    data_store_1d_t primal_normal_vert_v2_gt;
-    data_store_2d_t z_nabla2_e_gt;
-    data_store_1d_t inv_vert_vert_length_gt;
-    data_store_1d_t inv_primal_edge_length_gt;
-    data_store_2d_t z_nabla4_e2_wp_gt;
+    const data_store_2d_t u_vert_gt_tv;
+    const data_store_2d_t v_vert_gt_tv;
+    const data_store_1d_t primal_normal_vert_v1_gt_tv;
+    const data_store_1d_t primal_normal_vert_v2_gt_tv;
+    const data_store_2d_t z_nabla2_e_gt_tv;
+    const data_store_1d_t inv_vert_vert_length_gt_tv;
+    const data_store_1d_t inv_primal_edge_length_gt_tv;
+    const data_store_2d_t z_nabla4_e2_wp_gt_tv;
 
     std::vector<std::vector<VP_TYPE>> u_vert;
     std::vector<std::vector<VP_TYPE>> v_vert;
@@ -54,14 +54,6 @@ struct nabla4_data {
 
     /// Initialize vectors needed to execute kernel with random numbers
     void init_ifirst() {
-        u_vert_gt = storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build();
-        v_vert_gt = storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build();
-        primal_normal_vert_v1_gt = storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build();
-        primal_normal_vert_v2_gt = storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build();
-        z_nabla2_e_gt = storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build();
-        inv_vert_vert_length_gt = storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build();
-        inv_primal_edge_length_gt = storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build();
-        z_nabla4_e2_wp_gt = storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build();
         u_vert = rand_utils.random_init_vec_2d<VP_TYPE>(KDim, VertexDim);
         v_vert = rand_utils.random_init_vec_2d<VP_TYPE>(KDim, VertexDim);
         primal_normal_vert_v1 = rand_utils.random_init_vec_1d(EdgeDim * ECVDim);
@@ -90,7 +82,16 @@ struct nabla4_data {
     }
 
     nabla4_data(std::size_t CellDim, std::size_t VertexDim, std::size_t EdgeDim, std::size_t KDim, std::size_t ECVDim)
-        : CellDim(CellDim), VertexDim(VertexDim), EdgeDim(EdgeDim), KDim(KDim), ECVDim(ECVDim) {
+        : CellDim(CellDim), VertexDim(VertexDim), EdgeDim(EdgeDim), KDim(KDim), ECVDim(ECVDim),
+        u_vert_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        v_vert_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        primal_normal_vert_v1_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        primal_normal_vert_v2_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        z_nabla2_e_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        inv_vert_vert_length_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        inv_primal_edge_length_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        z_nabla4_e2_wp_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view())
+        {
         if constexpr (T == Data::ifirst) {
             init_ifirst();
         } else if constexpr (T == Data::kfirst) {
@@ -116,7 +117,15 @@ struct nabla4_data {
         : CellDim(CellDim), VertexDim(VertexDim), EdgeDim(EdgeDim), KDim(KDim), ECVDim(ECVDim), u_vert(u_vert),
           v_vert(v_vert), primal_normal_vert_v1(primal_normal_vert_v1), primal_normal_vert_v2(primal_normal_vert_v2),
           z_nabla2_e(z_nabla2_e), inv_vert_vert_length(inv_vert_vert_length),
-          inv_primal_edge_length(inv_primal_edge_length) {
+          inv_primal_edge_length(inv_primal_edge_length),
+        u_vert_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        v_vert_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, VertexDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        primal_normal_vert_v1_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        primal_normal_vert_v2_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim * ECVDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        z_nabla2_e_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()),
+        inv_vert_vert_length_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        inv_primal_edge_length_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(EdgeDim).initializer([](int i) { return rand_utils.template get<double>(); }).build()->target_view()),
+        z_nabla4_e2_wp_gt_tv(storage::builder<traits_t>.template type<double>().dimensions(KDim, EdgeDim).initializer([](int i, int j) { return rand_utils.template get<double>(); }).build()->target_view()) {
         if constexpr (T == Data::ifirst) {
             z_nabla4_e2_wp.resize(KDim);
             for (std::size_t i{}; i < KDim; ++i) {
