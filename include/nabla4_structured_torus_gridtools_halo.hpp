6ea5217a5fd72d06d1b5e5ccf5ef0defe81dd189
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-05-14 09:04:39 +0200

    Initial work for cooperative groups. Not fully working

--- include/nabla4_structured_torus_gridtools_halo.hpp
+++ include/nabla4_structured_torus_gridtools_halo.hpp
@@ -7,6 +7,8 @@
 #include "nabla4_gridtools.hpp"
 
 #if defined(__CUDACC__)
+#include <cooperative_groups.h>
+
 template <typename T>
 constexpr block_dims get_block_dims_structured() {
     throw std::runtime_error("Undefined block dimensions for type " + T::name + " in GPU backend");
@@ -14,7 +16,7 @@ constexpr block_dims get_block_dims_structured() {
 
 template <>
 constexpr block_dims get_block_dims_structured<std::size_t>() {
-    return {32, 3, 4, 384};
+    return {32, 3, 1, 96};
 };
 
 template <>
@@ -29,7 +31,7 @@ constexpr block_dims get_block_dims_structured<std::uint32_t>() {
 
 template <>
 constexpr block_dims get_block_dims_structured<int>() {
-    return {32, 3, 4, 384};
+    return {32, 3, 1, 96};
 };
 
 constexpr block_dims block_dims_structured = get_block_dims_structured<index_type>();
@@ -209,7 +211,7 @@ class nabla4_structured_torus_halo_gt : public nabla4_gt_data<T> {
 };
 
 #if defined(__CUDACC__)
-__global__ void __launch_bounds__(block_dims_structured.size, 2) run_gpu(index_type KDim,
+__global__ void __launch_bounds__(block_dims_structured.size) run_gpu(index_type KDim,
     index_type x_dim,
     index_type y_dim,
     index_type halo,
@@ -222,55 +224,74 @@ __global__ void __launch_bounds__(block_dims_structured.size, 2) run_gpu(index_t
     nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t inv_vert_vert_length_gt_tv,
     nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t inv_primal_edge_length_gt_tv,
     nabla4_structured_torus_halo_gt<storage::gpu>::data_store_2d_tv_VP_t z_nabla4_e2_wp_gt_tv) {
-    __shared__ index_type E2C2V[3][block_dims_structured.x][5];
+    namespace cg = cooperative_groups;
+    const int tid = cg::this_grid().thread_rank();
+    cg::cluster_group cluster = cg::this_cluster();
+    const unsigned int clusterBlockRank = cluster.block_rank();
+    const auto cluster_size = cluster.dim_blocks().z;
+    const auto cluster_index = cluster.block_index().z;
+    printf("tid: %d, Cluster block rank: %u, cluster block size: %u, thread global index: (%d, %d, %d)\n",
+        tid,
+        clusterBlockRank,
+        cluster_size,
+        blockIdx.x * blockDim.x + threadIdx.x,
+        blockIdx.y * blockDim.y + threadIdx.y,
+        blockIdx.z * blockDim.z + threadIdx.z);
+    __shared__ index_type E2C2V[3 * block_dims_structured.x * 4];
     __shared__ index_type is[block_dims_structured.x];
     __shared__ index_type js[block_dims_structured.x];
     const auto x_dim_without_halo = x_dim - 2 * halo;
-    for (auto edge_index{blockIdx.x * blockDim.x + threadIdx.x}; edge_index < total_grid_size;
-         edge_index += blockDim.x * gridDim.x) {
-        const auto i = edge_index % x_dim_without_halo + halo;
-        const auto j = (edge_index / x_dim_without_halo) + halo;
-        is[threadIdx.x] = i;
-        js[threadIdx.x] = j;
-        const auto i_j = j * x_dim + i;
-        if (threadIdx.y == 0) {
-            const auto i_jp1 = (j + 1) * x_dim + i;
-            const auto im1_jp1 = (j + 1) * x_dim + i - 1;
-            const auto ip1_j = j * x_dim + i + 1;
-            E2C2V[0][threadIdx.x][0] = i_j;
-            E2C2V[0][threadIdx.x][1] = i_jp1;
-            E2C2V[0][threadIdx.x][2] = im1_jp1;
-            E2C2V[0][threadIdx.x][3] = ip1_j;
-        } else if (threadIdx.y == 1) {
-            const auto ip1_j = j * x_dim + i + 1;
-            const auto i_jp1 = (j + 1) * x_dim + i;
-            const auto ip1_jm1 = (j - 1) * x_dim + i + 1;
-            E2C2V[1][threadIdx.x][0] = i_j;
-            E2C2V[1][threadIdx.x][1] = ip1_j;
-            E2C2V[1][threadIdx.x][2] = i_jp1;
-            E2C2V[1][threadIdx.x][3] = ip1_jm1;
-        } else {
-            const auto ip1_jm1 = (j - 1) * x_dim + i + 1;
-            const auto ip1_j = j * x_dim + i + 1;
-            const auto i_jm1 = (j - 1) * x_dim + i;
-            E2C2V[2][threadIdx.x][0] = i_j;
-            E2C2V[2][threadIdx.x][1] = ip1_jm1;
-            E2C2V[2][threadIdx.x][2] = ip1_j;
-            E2C2V[2][threadIdx.x][3] = i_jm1;
+    if (blockIdx.z == 0 && threadIdx.z == 0 && clusterBlockRank == 0) {
+        for (auto edge_index{blockIdx.x * blockDim.x + threadIdx.x}; edge_index < total_grid_size;
+             edge_index += blockDim.x * gridDim.x) {
+            const auto i = edge_index % x_dim_without_halo + halo;
+            const auto j = (edge_index / x_dim_without_halo) + halo;
+            is[threadIdx.x] = i;
+            js[threadIdx.x] = j;
+            const auto i_j = j * x_dim + i;
+            if (threadIdx.y == 0) {
+                const auto i_jp1 = (j + 1) * x_dim + i;
+                const auto im1_jp1 = (j + 1) * x_dim + i - 1;
+                const auto ip1_j = j * x_dim + i + 1;
+                E2C2V[0 * blockDim.x * 4 + threadIdx.x * 4 + 0] = i_j;
+                E2C2V[0 * blockDim.x * 4 + threadIdx.x * 4 + 1] = i_jp1;
+                E2C2V[0 * blockDim.x * 4 + threadIdx.x * 4 + 2] = im1_jp1;
+                E2C2V[0 * blockDim.x * 4 + threadIdx.x * 4 + 3] = ip1_j;
+            } else if (threadIdx.y == 1) {
+                const auto ip1_j = j * x_dim + i + 1;
+                const auto i_jp1 = (j + 1) * x_dim + i;
+                const auto ip1_jm1 = (j - 1) * x_dim + i + 1;
+                E2C2V[1 * blockDim.x * 4 + threadIdx.x * 4 + 0] = i_j;
+                E2C2V[1 * blockDim.x * 4 + threadIdx.x * 4 + 1] = ip1_j;
+                E2C2V[1 * blockDim.x * 4 + threadIdx.x * 4 + 2] = i_jp1;
+                E2C2V[1 * blockDim.x * 4 + threadIdx.x * 4 + 3] = ip1_jm1;
+            } else {
+                const auto ip1_jm1 = (j - 1) * x_dim + i + 1;
+                const auto ip1_j = j * x_dim + i + 1;
+                const auto i_jm1 = (j - 1) * x_dim + i;
+                E2C2V[2 * blockDim.x * 4 + threadIdx.x * 4 + 0] = i_j;
+                E2C2V[2 * blockDim.x * 4 + threadIdx.x * 4 + 1] = ip1_jm1;
+                E2C2V[2 * blockDim.x * 4 + threadIdx.x * 4 + 2] = ip1_j;
+                E2C2V[2 * blockDim.x * 4 + threadIdx.x * 4 + 3] = i_jm1;
+            };
         };
     };
+    cluster.sync();
+    index_type *dst_E2C2V = cluster.map_shared_rank(E2C2V, 0);
+    index_type *dst_is = cluster.map_shared_rank(is, 0);
+    index_type *dst_js = cluster.map_shared_rank(js, 0);
     const auto global_edges_per_orientation = x_dim * y_dim * 4;
     for (auto k_index{blockIdx.z * blockDim.z + threadIdx.z}; k_index < KDim; k_index += blockDim.z * gridDim.z) {
         for (auto orientation_id{blockIdx.y * blockDim.y + threadIdx.y}; orientation_id < 3;
              orientation_id += blockDim.y * gridDim.y) {
             for (auto edge_index{blockIdx.x * blockDim.x + threadIdx.x}; edge_index < total_grid_size;
                  edge_index += blockDim.x * gridDim.x) {
-                const auto E2C2V_0 = E2C2V[orientation_id][threadIdx.x][0];
-                const auto E2C2V_1 = E2C2V[orientation_id][threadIdx.x][1];
-                const auto E2C2V_2 = E2C2V[orientation_id][threadIdx.x][2];
-                const auto E2C2V_3 = E2C2V[orientation_id][threadIdx.x][3];
-                const auto i = is[threadIdx.x];
-                const auto j = js[threadIdx.x];
+                const auto E2C2V_0 = dst_E2C2V[orientation_id * blockDim.x * 4 + threadIdx.x * 4 + 0];
+                const auto E2C2V_1 = dst_E2C2V[orientation_id * blockDim.x * 4 + threadIdx.x * 4 + 1];
+                const auto E2C2V_2 = dst_E2C2V[orientation_id * blockDim.x * 4 + threadIdx.x * 4 + 2];
+                const auto E2C2V_3 = dst_E2C2V[orientation_id * blockDim.x * 4 + threadIdx.x * 4 + 3];
+                const auto i = dst_is[threadIdx.x];
+                const auto j = dst_js[threadIdx.x];
                 const auto global_edge_index = (j * x_dim + i) * 4;
                 const auto E2ECV_0 = global_edge_index + orientation_id * global_edges_per_orientation;
                 const auto E2ECV_1 = E2ECV_0 + 1;
@@ -301,8 +322,34 @@ template <typename T>
 inline void nabla4_structured_torus_halo_gt<T>::run_gpu_helper() {
     dim3 tblocks(block_dims_structured.x, block_dims_structured.y, block_dims_structured.z);
     const index_type total_grid_size = (x_dim - 2 * halo) * (y_dim - halo * 2);
-    dim3 grid((total_grid_size + tblocks.x - 1) / tblocks.x, 1, 1);
-    run_gpu<<<grid, tblocks>>>(KDim,
+    dim3 grid((total_grid_size + tblocks.x - 1) / tblocks.x, 1, 4);
+    printf("Grid dimensions: %d %d %d\n", grid.x, grid.y, grid.z);
+    cudaLaunchConfig_t config = {0};
+    config.gridDim = grid;
+    config.blockDim = tblocks;
+    cudaLaunchAttribute attribute[1];
+    attribute[0].id = cudaLaunchAttributeClusterDimension;
+    attribute[0].val.clusterDim.x = 1;
+    attribute[0].val.clusterDim.y = 1;
+    attribute[0].val.clusterDim.z = 4;
+    config.numAttrs = 1;
+    config.attrs = attribute;
+    void (*run_gpu_func)(index_type KDim,
+        index_type x_dim,
+        index_type y_dim,
+        index_type halo,
+        index_type total_grid_size,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_2d_ctv_VP_t u_vert_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_2d_ctv_VP_t v_vert_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t primal_normal_vert_v1_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t primal_normal_vert_v2_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_2d_ctv_WP_t z_nabla2_e_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t inv_vert_vert_length_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_1d_ctv_WP_t inv_primal_edge_length_gt_tv,
+        nabla4_structured_torus_halo_gt<storage::gpu>::data_store_2d_tv_VP_t z_nabla4_e2_wp_gt_tv) = run_gpu;
+    cudaLaunchKernelEx(&config,
+        run_gpu_func,
+        KDim,
         x_dim,
         y_dim,
         halo,
