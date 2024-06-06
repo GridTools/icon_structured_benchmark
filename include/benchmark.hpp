fd8d5f600d72c24022a52f832f9d3419c5288f3c
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-06-06 16:35:28 +0200

    Added naive versions for nabla4 structured and unstructured without k-blocking

--- include/benchmark.hpp
+++ include/benchmark.hpp
@@ -4,8 +4,10 @@
 #include "nabla4_structured_torus.hpp"
 #include "nabla4_structured_torus_gridtools.hpp"
 #include "nabla4_structured_torus_gridtools_halo.hpp"
+#include "nabla4_structured_torus_gridtools_halo_naive.hpp"
 #include "nabla4_unstructured.hpp"
 #include "nabla4_unstructured_gridtools.hpp"
+#include "nabla4_unstructured_gridtools_naive.hpp"
 #include "timer.hpp"
 
 template <typename T, backend_impl I>
