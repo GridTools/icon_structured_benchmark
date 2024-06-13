70cf55bc2e286d0876098e60eaf329581a19b44b
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-06-13 09:36:13 +0200

    Initial commit for structured

--- include/interpolate_structured_gridtools.hpp
+++ include/interpolate_structured_gridtools.hpp
@@ -46,42 +46,88 @@ class interpolate_structured : public mo_intp_rbf_rbf_vec_interpol_vertex<T> {
         : mo_intp_rbf_rbf_vec_interpol_vertex<T>(VertexDim, EdgeDim, KDim, p_e_in, ptr_coeff_1, ptr_coeff_2){};
 
   private:
-    void run_cpu_ifirst(){};
-    void run_cpu_kfirst(){};
-    void run_gpu_helper();
+    const std::array<index_type, 6> get_v2e(const int i, const int j) {
+        // i_j = i + j * x_dim
+        // i_jm1 = i + (j - 1) * x_dim
+        // v2e[j_internal * x_dim_internal + i_internal][0] = (x_dim * y_dim) + i_j - 1
+        // v2e[j_internal * x_dim_internal + i_internal][1] = (x_dim * y_dim) + i_j
+        // v2e[j_internal * x_dim_internal + i_internal][2] = i_jm1
+        // v2e[j_internal * x_dim_internal + i_internal][3] = i_j
+        // v2e[j_internal * x_dim_internal + i_internal][4] = 2 * (x_dim * y_dim) + i_j
+        // v2e[j_internal * x_dim_internal + i_internal][5] = (
+        //     2 * (x_dim * y_dim) + i_j + x_dim - 1
+        // )
+        const index_type i_j = i + j * x_dim;
+        const i_jm1 = i + (j - 1) * x_dim;
+        return {(x_dim * y_dim) + i_j - 1,
+            (x_dim * y_dim) + i_j,
+            i_jm1,
+            i_j,
+            2 * (x_dim * y_dim) + i_j,
+            2 * (x_dim * y_dim) + i_j + x_dim - 1};
+    };
+    void run_cpu_ifirst() {
+        for (index_type k_index{}; k_index < KDim; ++k_index) {
+            const auto vertices = v2e_gt_ctv.lengths()[0];
+#ifdef __clang__
+#pragma clang loop unroll(enable) vectorize(assume_safety) interleave(enable)
+#elif defined(__GNUC__)
+#pragma GCC ivdep
+#endif
+            for (index_type j = halo; j < y_dim - halo; ++j) {
+                for (index_type i = halo; i < x_dim - halo; ++i) {
+                    std::array<WP_TYPE, 6> u;
+                    std::array<WP_TYPE, 6> v;
+                    const std::array<index_type, 6> v2e{get_v2e(i, j)};
+#ifdef __clang__
+#pragma clang loop unroll(enable) vectorize(assume_safety) interleave(enable)
+#elif defined(__GNUC__)
+#pragma GCC ivdep
+#endif
+                    for (auto i{0}; i < 6; ++i) {
+                        u[i] = p_e_in_gt_ctv(v2e[i], k_index) * ptr_coeff_1_gt_ctv(vertex_index, i);
+                        v[i] = p_e_in_gt_ctv(v2e[i], k_index) * ptr_coeff_2_gt_ctv(vertex_index, i);
+                    }
+                    p_u_out_gt_tv(vertex_index, k_index) = std::accumulate(u.begin(), u.end(), 0.0);
+                    p_v_out_gt_tv(vertex_index, k_index) = std::accumulate(v.begin(), v.end(), 0.0);
+                };
+            };
+        };
+        void run_cpu_kfirst(){};
+        void run_gpu_helper();
 
-  public:
-    /// Compute function timed for benchmarking
-    template <backend_impl I>
-    inline void run() {
-        if constexpr (I == backend_impl::cpu_ifirst) {
-            run_cpu_ifirst();
-        } else if constexpr (I == backend_impl::cpu_kfirst) {
-            run_cpu_kfirst();
-        } else if constexpr (I == backend_impl::gpu) {
-            run_gpu_helper();
-        } else {
-            throw std::runtime_error("Undefined backend implementation");
+      public:
+        /// Compute function timed for benchmarking
+        template <backend_impl I>
+        inline void run() {
+            if constexpr (I == backend_impl::cpu_ifirst) {
+                run_cpu_ifirst();
+            } else if constexpr (I == backend_impl::cpu_kfirst) {
+                run_cpu_kfirst();
+            } else if constexpr (I == backend_impl::gpu) {
+                run_gpu_helper();
+            } else {
+                throw std::runtime_error("Undefined backend implementation");
+            }
         }
-    }
-};
+    };
 
 #if defined(__CUDACC__)
-__global__ void __launch_bounds__(block_dims_structured_interpol.size, 2) run_gpu_interpol(){};
+    __global__ void __launch_bounds__(block_dims_structured_interpol.size, 2) run_gpu_interpol(){};
 
-template <typename T>
-inline void interpolate_structured<T>::run_gpu_helper() {
-    // dim3 tblocks(block_dims_structured_interpol.x, block_dims_structured_interpol.y, block_dims_structured_interpol.z);
-    // const index_type inner_grid_size = (x_dim - 2 * halo) * (y_dim - halo * 2);
-    // dim3 grid((x_dim - 2 * halo + tblocks.x - 1) / tblocks.x,
-    //     (y_dim - 2 * halo + tblocks.y - 1) / tblocks.y,
-    //     (KDim + tblocks.z - 1) / tblocks.z);
-    // run_gpu_interpol<<<grid, tblocks>>>();
-    GT_CUDA_CHECK(cudaGetLastError());
-};
+    template <typename T>
+    inline void interpolate_structured<T>::run_gpu_helper() {
+        // dim3 tblocks(block_dims_structured_interpol.x, block_dims_structured_interpol.y,
+        // block_dims_structured_interpol.z); const index_type inner_grid_size = (x_dim - 2 * halo) * (y_dim - halo *
+        // 2); dim3 grid((x_dim - 2 * halo + tblocks.x - 1) / tblocks.x,
+        //     (y_dim - 2 * halo + tblocks.y - 1) / tblocks.y,
+        //     (KDim + tblocks.z - 1) / tblocks.z);
+        // run_gpu_interpol<<<grid, tblocks>>>();
+        GT_CUDA_CHECK(cudaGetLastError());
+    };
 #else
-template <typename T>
-inline void interpolate_structured<T>::run_gpu_helper() {
-    throw std::runtime_error("GPU backend not enabled");
-};
+    template <typename T>
+    inline void interpolate_structured<T>::run_gpu_helper() {
+        throw std::runtime_error("GPU backend not enabled");
+    };
 #endif
