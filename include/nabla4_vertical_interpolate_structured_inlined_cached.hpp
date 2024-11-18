c2a1859b5adca80316c1ef279d6283eb85afb51c
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-11-18 19:42:35 +0100

    Improved gpu_naive inlined_cached version for vertical

--- include/nabla4_vertical_interpolate_structured_inlined_cached.hpp
+++ include/nabla4_vertical_interpolate_structured_inlined_cached.hpp
@@ -297,7 +297,7 @@ constexpr block_dims get_block_dims_structured_nabla_interpol_inlined_cached_nai
 
 template <>
 constexpr block_dims get_block_dims_structured_nabla_interpol_inlined_cached_naive_vertical<int>() {
-    return {32, 8, 1, 256};
+    return {32, 8, 2, 512};
 };
 
 constexpr block_dims block_dims_structured_nabla_interpol_inlined_cached_naive_vertical =
@@ -307,7 +307,7 @@ __global__ void
 #if __CUDACC_VER_MAJOR__ < 12 || (__CUDACC_VER_MAJOR__ == 12 && __CUDACC_VER_MINOR__ < 5)
 __launch_bounds__(block_dims_structured_nabla_interpol_inlined_cached_naive_vertical.size)
 #else
-__maxnreg__(64)
+__maxnreg__(52)
 #endif
     run_gpu_naive_nabla4_vertical_interpolate_inlined_cached_structured(index_type KDim,
         index_type x_dim,
