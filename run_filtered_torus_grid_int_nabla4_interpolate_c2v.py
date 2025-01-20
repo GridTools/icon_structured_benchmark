5272141d9f812adeec2c6dfd94aa729901a8e1dd
Ioannis Magkanaris <iomagkanaris@gmail.com>
2025-01-20 13:59:42 +0100

    Execute inlined_v2v_separate for gpu_naive as well

--- run_filtered_torus_grid_int_nabla4_interpolate_c2v.py
+++ run_filtered_torus_grid_int_nabla4_interpolate_c2v.py
@@ -434,6 +434,34 @@ def run_sanity_checks(
                 "structured gpu_naive nabla4_interpolate_inlined separate sanity check passed"
             )
 
+            print("Running unstructured gpu_naive inlined_v2v_separate sanity check")
+            p_cell_out_unstructured_gpu_naive_inlined_v2v_separate = icon_benchmark.nabla4_interpolate_inlined_v2v_verts2cells_validate_unstructured_gpu_naive_separate(
+                filtered_e2c2v_inlined,
+                filtered_e2ecv_inlined,
+                filtered_v2e_inlined,
+                filtered_c2v_inlined,
+                random_validation_data_separate.CellDim,
+                random_validation_data_separate.VertexDim,
+                random_validation_data_separate.EdgeDim,
+                random_validation_data_separate.KDim,
+                random_validation_data_separate.ECVDim,
+                np.array(random_validation_data_separate.u_vert).T,
+                np.array(random_validation_data_separate.v_vert).T,
+                random_validation_data_separate.primal_normal_vert_v1,
+                random_validation_data_separate.primal_normal_vert_v2,
+                z_nabla2_e_inlined,
+                inv_vert_vert_length_inlined,
+                inv_primal_edge_length_inlined,
+                ptr_coeff_1,
+                ptr_coeff_2,
+                ptr_c_coeff_1,
+                ptr_c_coeff_2,
+            )
+            assert np.allclose(
+                p_cell_out_unstructured_gpu_naive_inlined_v2v_separate, p_cell_out_ref
+            )
+            print("unstructured gpu_naive inlined_v2v_separate sanity check passed")
+
         if combination in ["all", "inlined"]:
             print("Running unstructured gpu_naive inlined sanity check")
             p_cell_out_unstructured_gpu_naive_inlined = icon_benchmark.nabla4_interpolate_verts2cells_validate_unstructured_gpu_naive_inlined(
@@ -1222,6 +1250,30 @@ def run_benchmarks():
                     np.median(
                         runtimes[
                             "nabla4_interpolate_inlined_verts2cells_benchmark_structured_gpu_naive_separate"
+                        ]
+                    )
+                )
+            )
+            runtimes[
+                "nabla4_interpolate_inlined_v2v_verts2cells_benchmark_unstructured_gpu_naive_separate"
+            ] = icon_benchmark.nabla4_interpolate_inlined_v2v_verts2cells_benchmark_unstructured_gpu_naive_separate(
+                filtered_e2c2v_inlined,
+                filtered_e2ecv_inlined,
+                filtered_v2e_inlined,
+                filtered_c2v,
+                torus_grid.num_cells,
+                torus_grid.num_vertices,
+                torus_grid.num_edges,
+                torus_grid.num_levels,
+                torus_grid.size[E2C2VDim],
+                repetitions,
+                dry_runs,
+            )
+            print(
+                "nabla4_interpolate_inlined_v2v_verts2cells_benchmark_unstructured_gpu_naive_separate median: {}".format(
+                    np.median(
+                        runtimes[
+                            "nabla4_interpolate_inlined_v2v_verts2cells_benchmark_unstructured_gpu_naive_separate"
                         ]
                     )
                 )
