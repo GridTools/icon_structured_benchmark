48d902bbeb5ab3dec52959831fcba97718e36c3d
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-01-24 16:56:33 +0100

    Fixing installation of wheel

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description="A Python package with a CMake project",
     author="Your Name",
     # packages=["."],  # Replace with your package name
-    cmake_install_dir="build_skbuild",  # Directory to install built files
+    # cmake_install_dir="build_skbuild",  # Directory to install built files
 )
