e2b644080dfaf1e563dcc086db619a33c9c05e37
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-01-24 17:58:35 +0100

    Fixed steup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from skbuild import setup
 
 setup(
-    name="icon_structured_benchmarks",
+    name="icon_structured_benchmark",
     version="0.1.0",
     description="A Python package with a CMake project",
     author="Your Name",
+    packages=["icon_benchmark"],  # Replace with your package name
+    package_dir={"icon_benchmark": "python"},
+    cmake_install_dir="python",
     # packages=["."],  # Replace with your package name
     # cmake_install_dir="build_skbuild",  # Directory to install built files
 )
