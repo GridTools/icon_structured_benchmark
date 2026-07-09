47e34bf1c9e2285c81e50b3547bcd275910eb42d
Ioannis Magkanaris <iomagkanaris@gmail.com>
2026-07-09 13:42:32 +0200

    Add device synchronize in the benchmark loop

--- include/benchmark.hpp
+++ include/benchmark.hpp
@@ -40,6 +40,10 @@
 #include "verts2cells_structured_gridtools.hpp"
 #include "verts2cells_unstructured_gridtools.hpp"
 
+#ifdef __CUDACC__
+#include "cuda_runtime_api.h"
+#endif
+
 template <typename T, backend_impl I>
 std::vector<double> run_benchmark(T &benchmark_object, int repetitions, int dry_runs) {
     for (int dry_run{}; dry_run < dry_runs; ++dry_run) {
@@ -62,11 +66,21 @@ std::vector<double> run_benchmark(std::tuple<Args...> &&args, int repetitions, i
     for (int dry_run{}; dry_run < dry_runs; ++dry_run) {
         benchmark_object.template run<I>();
     }
+#ifdef __CUDACC__
+    if constexpr (I == backend_impl::gpu_kloop || I == backend_impl::gpu_naive) {
+        GT_CUDA_CHECK(cudaDeviceSynchronize());
+    }
+#endif
     std::vector<double> runtimes;
     for (int rep{}; rep < repetitions; ++rep) {
         timer<I> t;
         t.start();
         benchmark_object.template run<I>();
+#ifdef __CUDACC__
+        if constexpr (I == backend_impl::gpu_kloop || I == backend_impl::gpu_naive) {
+            GT_CUDA_CHECK(cudaDeviceSynchronize());
+        }
+#endif
         t.stop();
         runtimes.push_back(t.elapsed());
     }
